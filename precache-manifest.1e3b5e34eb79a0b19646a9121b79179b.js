8e78ebe323f8b6e2ed3fb377472ce0fc23c30735
Mohd Yunus <112767616+Yunuslala@users.noreply.github.com>
2023-08-30 14:27:00 +0530

    Updates

---
+++ precache-manifest.1e3b5e34eb79a0b19646a9121b79179b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c57512df995ca42c097fdc87b6e7c44",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "8a6df79e03c8eb5bbf3d",
+    "url": "/codepen-clone/static/css/main.8a79c027.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6df79e03c8eb5bbf3d",
+    "url": "/codepen-clone/static/js/main.5374e446.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
